11d95ca0b0928dd958e8cd0737e8d345aa2b3fe6
Ethan Damien <ethanchang.d@gmail.com>
2021-04-09 22:47:16 -0400

    Added Hello World

--- animaltest/Main.java
+++ animaltest/Main.java
@@ -5,18 +5,20 @@ class Main {
       //You can make Animal Class and give it a name
       //You can set dog name in constructor
       //You can also get the name by calling getName()
-      String dogname = "dog";
-      String dogsound = "bark";
+      // String dogname = "dog";
+      // String dogsound = "bark";
       // System.out.println(dogname);
 
       //System.out.println("dog");
 
-      Animal dog = new Animal("Dog", "Bark");
-      dog.getName();
-      System.out.println(dog.getSound());
-      System.out.println(dog.getheightplusweight());
+      // Animal dog = new Animal("Dog", "Bark");
+      // dog.getName();
+      // System.out.println(dog.getSound());
+      // System.out.println(dog.getheightplusweight());
 
-      Animal cat = new Animal("Mittens", "Meow");
-      cat.getName(); 
+      // Animal cat = new Animal("Mittens", "Meow");
+      // cat.getName(); 
+
+      System.out.println("Hello World");
     }
   }
