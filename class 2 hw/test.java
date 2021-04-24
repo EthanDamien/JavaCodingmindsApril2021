3565184ba529a3391526584dd7a6d9434aae8f33
JoshuaTian13 <joshuatian.ca@gmail.com>
2021-04-23 18:22:29 -0700

    class 2 hw

---
+++ class 2 hw/test.java
@@ -0,0 +1,14 @@
+public class test {
+
+    public static void main(String args[]) {
+
+        int num1 = 7;
+        System.out.println(5 + num1);
+        num1 -= 4;
+        System.out.println(num1);
+        String num = Integer.toString(num1);
+        num+=1;
+        System.out.println(num);
+    }
+
+}
