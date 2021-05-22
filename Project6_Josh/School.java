0452d8ddae0ccdc91ef417da944f5414cad997f4
Ethan Damien <ethanchang.d@gmail.com>
2021-05-21 21:28:32 -0400

    commit

--- Project6_Josh/School.java
+++ Project6_Josh/School.java
@@ -4,17 +4,32 @@ public class School {
         String[] students = {"James", "Kevin", "Donald", "Josh", "Arnold", "Casey", "C3Codingminds"};
         double[] gpa = {1.1,2.5,3.3,7.3,6.9,2,100};
 
+        System.out.println(getGPA("poop", students, gpa));
+        updateGPA("C3Codingminds", students, gpa, 5.3);
         System.out.println(getGPA("C3Codingminds", students, gpa));
 
     }
     public static double getGPA(String name, String[] students, double[] gpa){
         int position = 0;
-        for(int i = 0;i<7;i++){
+        for(int i = 0;i<students.length;i++){
             if (students[i].equals(name)){
                 position = i;
-                break;
+                return gpa[i];
             }
         }
-        return gpa[position];
+        System.out.println("This person does not exist");
+        return -1;
     }
+
+    public static void updateGPA(String name, String[] students, double[] gpa, double newGPA){
+        for(int i = 0;i<students.length;i++){
+            if (students[i].equals(name)){
+                gpa[i] = newGPA;
+                return;
+            }
+        }
+        System.out.println("Error: This person Does not Exist");
+    }
+
+    
 }
