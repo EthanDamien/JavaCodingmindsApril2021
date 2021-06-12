09b7381794fa66cf3e869cfe4ba1d512a96ca73d
JoshuaTian13 <joshuatian.ca@gmail.com>
2021-06-11 18:03:02 -0700

    hi

---
+++ Project6_Josh/updategpa.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class UpdateGPA {
+    public static void main(String args[]){
+        String[] students = {"James", "Kevin", "Donald", "Josh", "Arnold", "Casey", "C3Codingminds"};
+        double[] gpa = {1.1, 2.5, 3, 7.3, 6.9, 2, 100};
+
+    }
+
+    public static void Update(String args[]){
+        
+    }
+}
--- Project8_Josh/randomstuff.java
+++ Project8_Josh/randomstuff.java
@@ -4,12 +4,14 @@ public class randomstuff {
     public static void main(String args[]){
         // int num = random(-100, 100);
         // System.out.println(num+" " + yes(num));
+        int[][] numberss = {{1,2,3,4,5,1,10,4}, {5,2,3,4,1,12,9,7}, {21, 54, 1, -5, 7, 8, 13}};
         int[] numbers = {1,2,3,4,5,1,10,4};
-        System.out.println(minimum(numbers));
-        System.out.println(maximum(numbers));
-        System.out.println(average(numbers));
-        System.out.println(areaOfSquare(10));
-        System.out.println(surfaceAreaOfCube(10));
+        System.out.println(maximum2d(numberss));
+        // System.out.println(minimum(numbers));
+        // System.out.println(maximum(numbers));
+        // System.out.println(average(numbers));
+        // System.out.println(areaOfSquare(10));
+        // System.out.println(surfaceAreaOfCube(10));
     }
     public static int random(int x, int y){
         int rand = (int)(Math.random()*((y+1)-x));
@@ -57,6 +59,17 @@ public class randomstuff {
     }
     public static int surfaceAreaOfCube(int side){
         return (6* areaOfSquare(side));
+    }
+    public static int maximum2d(int numberss[][]){
+        int max = -2147483648;
+        for(int i = 0; i<numberss.length; i++){
+            for(int j = 0; j<numberss[i].length; j++){
+                if (numberss[i][j] > max){
+                    max = numberss[i][j];
+                }
+            }
+        }
+        return max;
     }
     
 }
